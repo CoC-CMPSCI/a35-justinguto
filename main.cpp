b0a694094ac0c24683aa2e80b752f8af2b9c2c19
justinguto <163184050+justinguto@users.noreply.github.com>
2026-03-16 16:30:12 -0700

    Refactor random number generation and output logic

--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-#include <iomanip>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
 
 int main()
 {
-    int rdnum1, rdnum2, rdnum3;
-    int total = 0;
+    int rdnum1, rdnum2, rdnum3, total = 0;
 
-    // TODO: seed the random number generator with time(0)
+    srand(time(0));
 
-    // TODO: generate 3 random numbers between 0 and 99
+    rdnum2 = rand() % 100;
+    rdnum3 = rand() % 100;
 
-    // TODO: compute the total summation
+    total = rdnum1 + rdnum2 + rdnum3;
 
-    // TODO: print the 3 random numbers on the first line
+    cout << rdnum1 << " " << rdnum2 << " " << rdnum3 << endl;
 
-    // TODO: print the total and average (integer division) on the second line
+    cout << total << " " << total / 3 << endl;
 
     return 0;
 }
